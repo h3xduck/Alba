3ec393f792807747acfa44f86553822effc266ca
Marcos <marcossanchezbajo@gmail.com>
2020-07-29 22:40:26 +0200

    Completed google's codelabs to learn about architecture components. Implemented a database with room, a viewmodel, a repository and a custom viewmodelfactory as a workaround of some errors. Eeverything is functional but needs to be adapted to Alba.

--- app/src/main/java/com/marsanpat/alba/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/marsanpat/alba/ui/dashboard/DashboardFragment.java
@@ -1,35 +1,98 @@
 package com.marsanpat.alba.ui.dashboard;
 
+import android.app.Application;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.marsanpat.alba.Database.Word;
+import com.marsanpat.alba.MainActivity;
+import com.marsanpat.alba.NewWordActivity;
 import com.marsanpat.alba.R;
+
+import java.util.List;
+
+import static android.app.Activity.RESULT_OK;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel dashboardViewModel;
+    public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
+
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        RecyclerView recyclerView = root.findViewById(R.id.recyclerview);
+        final WordListAdapter adapter = new WordListAdapter(getContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        dashboardViewModel = new ViewModelProvider(this, new MyViewModelFactory(this.getActivity().getApplication())).get(DashboardViewModel.class);
+        dashboardViewModel.getAllWords().observe(getViewLifecycleOwner(), new Observer<List<Word>>() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onChanged(@Nullable final List<Word> words) {
+                // Update the cached copy of the words in the adapter.
+                adapter.setWords(words);
             }
         });
+
+        FloatingActionButton fab = root.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), NewWordActivity.class);
+                startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
+            }
+        });
+
+
+
         return root;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            Word word = new Word(data.getStringExtra(NewWordActivity.EXTRA_REPLY));
+            dashboardViewModel.insert(word);
+        } else {
+            Toast.makeText(
+                    getContext(),
+                    R.string.empty_not_saved,
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public static class MyViewModelFactory implements ViewModelProvider.Factory {
+        private final Application myApplication;
+
+        public MyViewModelFactory(Application application) {
+            this.myApplication = application;
+        }
+
+        @NonNull
+        @Override
+        @SuppressWarnings("unchecked")
+        public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+            return (T) new DashboardViewModel(myApplication);
+        }
     }
 }
--- app/src/main/java/com/marsanpat/alba/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/com/marsanpat/alba/ui/dashboard/DashboardViewModel.java
@@ -1,19 +1,30 @@
 package com.marsanpat.alba.ui.dashboard;
 
+import android.app.Application;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.marsanpat.alba.Database.Word;
+import com.marsanpat.alba.Database.WordRepository;
+
+import java.util.List;
+
 public class DashboardViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    private WordRepository mRepository;
 
-    public DashboardViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+    private LiveData<List<Word>> mAllWords;
+
+    public DashboardViewModel (Application application) {
+
+        mRepository = new WordRepository(application);
+        mAllWords = mRepository.getAllWords();
     }
 
-    public LiveData<String> getText() {
-        return mText;
-    }
+
+    LiveData<List<Word>> getAllWords() { return mAllWords; }
+
+    public void insert(Word word) { mRepository.insert(word); }
 }
