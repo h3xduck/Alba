34eca32f4e14f919e4ca21ff2a6fbf913f161540
Marcos <marcossanchezbajo@gmail.com>
2020-09-12 13:27:32 +0200

    Finished settings fragment main functionality (need to add more settings and make them work tho).

--- app/src/main/java/com/marsanpat/alba/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/marsanpat/alba/ui/settings/SettingsFragment.java
@@ -1,15 +1,33 @@
 package com.marsanpat.alba.ui.settings;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.preference.EditTextPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 import com.marsanpat.alba.R;
 
 public class SettingsFragment extends PreferenceFragmentCompat {
 
+    private EditTextPreference server_ip_preference;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        server_ip_preference = (EditTextPreference)  getPreferenceManager().findPreference("server_ip");
+        server_ip_preference.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                return true;
+            }
+        });
     }
+
+
 }
